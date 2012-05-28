2c97ed91a52c714ec9624e0c85491e2c27c38910
Janson Kang <temasek71@gmail.com>
2012-05-28 12:34:03 +0800

    Update to Linux v2.6.32.59

--- include/linux/regset.h
+++ include/linux/regset.h
@@ -336,7 +336,7 @@ static inline int copy_regset_to_user(struct task_struct *target,
 	const struct user_regset *regset = &view->regsets[setno];
 
 	if (!access_ok(VERIFY_WRITE, data, size))
-		return -EIO;
+		return -EFAULT;
 
 	return regset->get(target, regset, offset, size, NULL, data);
 }
@@ -359,7 +359,7 @@ static inline int copy_regset_from_user(struct task_struct *target,
 	const struct user_regset *regset = &view->regsets[setno];
 
 	if (!access_ok(VERIFY_READ, data, size))
-		return -EIO;
+		return -EFAULT;
 
 	return regset->set(target, regset, offset, size, NULL, data);
 }
